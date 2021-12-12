73d58f1ed222b438a7cedc5591b5f28d863d5246
Eduardo <95986173+EduF1g@users.noreply.github.com>
2021-12-11 20:19:42 -0600

    Add files via upload

---
+++ hola.cpp
@@ -0,0 +1,17 @@
+/*
+ * Programacion C++
+ * Saluda por primera vez, primer programa
+ * 
+ * EduF1g 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+cout<<"¡Hola que tal!";
+
+return 0;
+}
