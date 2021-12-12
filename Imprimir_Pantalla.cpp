ce94a6a2e08306d5c359dcf2856265683688c964
Eduardo <95986173+EduF1g@users.noreply.github.com>
2021-12-11 20:21:46 -0600

    Add files via upload

---
+++ Imprimir_Pantalla.cpp
@@ -0,0 +1,42 @@
+/* Programacion C++
+ * 
+ * Funcion que manda a imprimir en pantalla
+ * 
+ * EduF1g
+ */
+
+
+
+#include <iostream>
+using namespace std;
+
+void imprimir_pantalla();
+
+int main()
+{
+
+imprimir_pantalla();
+
+return 0;
+}
+
+void imprimir_pantalla()
+{
+ cout<<"                      .'  '."<<endl;
+ cout<<"                 _.-'/  |  \\"<<endl;
+ cout<<"    ,          .-'' ,|  /  0 `-. "<<endl;
+ cout<<"    |\\    .-''       `--''-.__.'=====================-,"<<endl;
+ cout<<"    \\ '-'`        .___.--._)=========================|"<<endl;
+ cout<<"     \\            .'      |                          |"<<endl;
+ cout<<"      |     /,_.-'        |       WELCOME            |"<<endl;
+ cout<<"    _/   _.'(             |              TO          |"<<endl;
+ cout<<"   /  ,-' \\ \\             |                C++       |"<<endl;
+ cout<<"   \\  \\    `-'            |                          |"<<endl;
+ cout<<"    `-'                   '--------------------------'"<<endl; 
+	
+	
+	
+	
+	
+	
+	}
