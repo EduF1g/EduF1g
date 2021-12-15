ee9dfa20933c8b723bc044b0b094a18579f8e70d
Eduardo <95986173+EduF1g@users.noreply.github.com>
2021-12-14 20:40:07 -0600

    Ciclo For
    Definición y declaración ciclo for

---
+++ ciclo_for.cpp
@@ -0,0 +1,25 @@
+/*
+ * Programacion C++
+ * 
+ * Ciclo For
+ * Un ciclo for es una estructura de control iterativa, que nos permite
+ * ejecutar de manera repetitiva un bloque de instrucciones, conociendo
+ * previamente un valor de inicio, un tamaño de paso y un valor final 
+ * para el ciclo
+ * 
+ * EduF1g
+ */
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	for(int i; i<=10;i++)
+
+		cout<<"El valor de i es: "<<i<<endl;
+
+	return 0;
+}
