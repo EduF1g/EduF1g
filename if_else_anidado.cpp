1310d6ec2ad7ba5c01932869e51203c037060996
Eduardo <95986173+EduF1g@users.noreply.github.com>
2021-12-17 22:51:16 -0600

    Anidar bloque if-else
    Ejemplo de uso de un if - else anidado

---
+++ if_else_anidado.cpp
@@ -0,0 +1,59 @@
+/*
+ * Programacion C++
+ * 
+ * Ejemplo de uso del condicional if -else aninado:
+ * Calculo del aumento del salario ingresado.
+ * 
+ * EduF1g
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+float sueldo, total, aumento;
+
+
+int main()
+{
+	cout<<"AUMENTO DE SUELDOS"<<endl;
+	cout<<"15% AUMENTO SALARIO MENOR A 2000"<<endl;
+	cout<<"10% AUMENTO SALARIO MENOR A 5000"<<endl;
+	cout<<"5% AUMENTO SALARIO MAYOR A 5000"<<endl;
+	
+	cout<<endl;
+	cout<<endl;
+
+	cout<<"Ingrese el salario: "<<endl;
+	cin>>sueldo;
+	
+	if (sueldo<2000)
+	{
+		aumento=sueldo*0.15;
+	}
+	else 
+	{
+	   if (sueldo<5000)
+	   {
+		   aumento=sueldo*0.10;
+	   }
+   
+	    else
+	      {
+			  aumento=sueldo *0.05;
+		  }
+	  
+    }
+	total=aumento +  sueldo;
+	
+	cout<<"Salario : "<<sueldo<<endl;
+	cout<<"Aumento : "<<aumento<<endl;
+	cout<<"Salario Neto: "<<total<<endl;	  
+
+	return 0;
+}
+
+	
+	
+	
