eb34b038512b9739d0ffea9281c229eddadb6ceb
Eduardo <95986173+EduF1g@users.noreply.github.com>
2022-01-05 21:11:43 -0600

    Definición y uso de cin.getline()
    Función para obtener string  ingresado por teclado por el usuario

---
+++ cin.getline.cpp
@@ -0,0 +1,29 @@
+/*
+ *Programacion C++ 
+ * 
+ * Uso de cin.getline(), cin.get()
+ * 
+ * cin.getline() obtiene un string ingresado por teclado. Necesita tres
+ * parametros:
+ * 			1. Nombre de variable que contiene el string
+ * 			2. Cantidad de caracteres que deseemos se puedan introducir
+ * 			3. El caracter que el usuario va a usar como final de la 
+ * 			   cadena, generamente es '\n' (Enter)
+ * 
+ * EduF1g
+ */
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char str[500];
+	cout<<"Introduce una frase: ";
+	cin.getline(str,500, '\n');// '\n' es salto de linea o enter
+	cout<<"Tu frase es: "<<str;
+	cin.get();
+}
